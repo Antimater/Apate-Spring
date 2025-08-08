26751493e4f3e155d2c2f5c99a7526e27223aecd
marce <marce@PC1.fritz.box>
2025-08-08 13:15:23 +0200

    project basic init

---
+++ src/main/java/com/example/Apate_Spring/ApateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Apate_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApateSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Apate_Spring/ApateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apate_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
